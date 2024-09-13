c321f8dfbf233e80c0170755133f1b13db9a6c14
wafi <34021031+wafi-izz@users.noreply.github.com>
2024-09-13 22:48:37 +0300

    init

--- src/ArithmeticOperatorsTester.java
+++ src/ArithmeticOperatorsTester.java
@@ -70,6 +70,9 @@ class ArithmeticOperators {
      * @return the int
      */
     public int div(int num1, int num2) {
+        if (num2 == 0) {
+            return 0;
+        }
         return num1 / num2;
     }
 }
